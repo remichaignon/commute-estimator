db836c8c7e6da8335872691c5bb8b6f2a058ed86
Rémi <remi.chaignon@gmail.com>
2014-09-04 00:48:49 -0600

    use latest firebase rsvp

--- commute-estimator.js
+++ commute-estimator.js
@@ -1,6 +1,7 @@
 var jsdom = require("jsdom"),
+    Firebase = require("firebase"),
     fs = require("fs"),
-    moment = require("moment")
+    moment = require("moment"),
     RSVP = require("rsvp"),
     schedule = require("node-schedule");
 
@@ -9,6 +10,9 @@ var Commutes = require("./commutes"),
     // Experiments = require("./experiments"),
     Readings = require("./readings"),
     Users = require("./users");
+
+
+// UTILS
 
 function buildErrorMessage (error) {
     if (!error) return "unknown";
@@ -19,22 +23,27 @@ function logError (error) {
     console.log("ERROR - ", buildErrorMessage(error));
 };
 
+
+// VARIABLES
+
 var DB_ROOT_URL = "https://commute-estimator.firebaseio.com/";
 
-var users = new Users(DB_ROOT_URL),
-    commutes = new Commutes(DB_ROOT_URL),
-    readings = new Readings(DB_ROOT_URL);//,
-    // estimations = new Estimations(DB_ROOT_URL),
-    // experiments = new Experiments(DB_ROOT_URL);
 
-users.get("jessica")
+// START
+
+var firebaseRef = new Firebase(DB_ROOT_URL);
+
+Users.get(firebaseRef, "jessica")
     .then(function (jessica) {
         console.log(jessica);
-        return users.getCommutes(jessica.username);
-    })
-    .then(function (jessicaCommutes) {
-        return commutes.get(jessicaCommutes[0]);
-    })
-    .then(function (commute) {
-        return commutes.scheduleReadings(commute.name)
     });
+    // .then(function (jessica) {
+    //     console.log(jessica);
+    //     return users.getCommutes(jessica.username);
+    // })
+    // .then(function (jessicaCommutes) {
+    //     return commutes.get(jessicaCommutes[0]);
+    // })
+    // .then(function (commute) {
+    //     return commutes.scheduleReadings(commute.name)
+    // });
--- users.js
+++ users.js
@@ -11,53 +11,43 @@ var _ = require("underscore"),
 //     commutes: {}
 // }
 
-var Users = function (databaseRootURL) {
-    this.path = "users";
-    this.uniqueKey = "username";
+FirebaseRSVP.namespace = "users";
+
+var Users = _.extend(FirebaseRSVP, {
+    DEFAULT_TIME_TO_GET_READY: 30,
+    DEFAULT_USER: { time_to_get_ready: this.DEFAULT_TIME_TO_GET_READY },
+
+    uniqueKey: "username",
 
     // Users methods
 
-    this.get = function (uniqueKey) {
-        return _users.get(uniqueKey);
-    };
-    this.set = function (uniqueKey, options) {
-        return _users.set(uniqueKey, options);
-    };
-    this.update = function (uniqueKey, options) {
-        return _users.update(uniqueKey, options);
-    };
-    this.remove = function (uniqueKey) {
-        return _users.remove(uniqueKey);
-    };
-    this.create = function (uniqueKey, options) {
+    create: function (ref, uniqueKey, options) {
         options = options || _.extend({}, DEFAULT_USER);
         options[this.uniqueKey] = uniqueKey;
 
-        return this.set(uniqueKey, options);
-    };
+        return FirebaseRSVP.set(ref, uniqueKey, options);
+    },
 
     // Commute-related methods
 
-    this.buildPathForCommute = function (uniqueKey, commute) {
-        return [uniqueKey, "commutes", commute.name].join("/");
-    };
-    this.addCommute = function (uniqueKey, commute) {
-        return this.set(this.buildPathForCommute(uniqueKey, commute), true);
-    };
-    this.getCommutes = function (uniqueKey) {
-        return this.get(uniqueKey + "/commutes")
+    refToCommute: function (ref, uniqueKey) {
+        return ref.child([uniqueKey, "commutes"].join("/"));
+    },
+    refToCommutes: function (ref, uniqueKey, commute) {
+        return ref.child([uniqueKey, "commutes", commute.name].join("/"));
+    },
+    addCommute: function (ref, uniqueKey, commute) {
+        return refToCommute(ref, uniqueKey).set(commute.name, true);
+    },
+    getCommutes: function (ref, uniqueKey) {
+        return refToCommutes()
             .then(function (commutes) {
                 return Object.keys(commutes);
             });
-    };
-    this.removeCommute = function (uniqueKey, commute) {
-        return this.remove(this.buildPathForCommute(uniqueKey, commute));
-    };
-
-    var _users = new FirebaseRSVP(databaseRootURL + this.path);
-
-    var DEFAULT_TIME_TO_GET_READY = 30;
-    var DEFAULT_USER = { time_to_get_ready: DEFAULT_TIME_TO_GET_READY };
-};
+    },
+    removeCommute: function (ref, uniqueKey, commute) {
+        return refToCommute(ref, uniqueKey, commute).remove();
+    }
+});
 
 module.exports = Users;
