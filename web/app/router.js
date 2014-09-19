7852e07f89a83beb60f8c9b1f8ba7d1fbedfc551
Rémi <remi.chaignon@gmail.com>
2014-09-18 23:49:06 -0600

    Properly add emberfire

--- web/app/router.js
+++ web/app/router.js
@@ -6,6 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+    this.resource("index", { path: "/" });
+
+    this.resource("user", { path: ":username" });
 });
 
 export default Router;
