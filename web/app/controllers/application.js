6dd67046f5c30eae11adfb56941f6deed627740f
Rémi <remi.chaignon@gmail.com>
2014-10-13 22:56:08 -0600

    Missing semi-colons

--- web/app/controllers/application.js
+++ web/app/controllers/application.js
@@ -11,11 +11,11 @@ export default Ember.Controller.extend({
                 throw new Error("ERROR - User invalid.");
             }
 
-            this.get("session").set("user", username)
+            this.get("session").set("user", username);
             this.transitionToRoute("user", username);
         },
         stop: function () {
-            this.set("username", "")
+            this.set("username", "");
             this.get("session").set("user", "");
             this.transitionToRoute("index");
         }
